d771813fcee9d4f57490b47b8be961a9b02f1fa7
JIShelley <jonathanishelley@gmail.com>
2026-09-21 09:41:02 -0600

    if statements

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour tout le monde.");
-        Console.WriteLine("Bom dia.");
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        if (x==10 || y == 21 && z == 30)
+        {
+            Console.WriteLine("x is 10");
+            Console.WriteLine("y is fun");
+        }
+
+        else if (x == 20)
+        {
+            Console.WriteLine("We're in the else if");
+        }
+
+        else
+        {
+            Console.WriteLine("Z is not much fun");
+        }
     }
 }
